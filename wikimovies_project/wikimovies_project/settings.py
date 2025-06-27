cbe672c8c3290063c85730c022cc2e9377373667
yashasnadigsyn <yashasnadigsyn@protonmail.com>
2025-06-27 14:50:10 +0530

    Added till scraping info - Should add plot and cast

--- wikimovies_project/wikimovies_project/settings.py
+++ wikimovies_project/wikimovies_project/settings.py
@@ -86,7 +86,7 @@ AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+# HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = "httpcache"
 #HTTPCACHE_IGNORE_HTTP_CODES = []
